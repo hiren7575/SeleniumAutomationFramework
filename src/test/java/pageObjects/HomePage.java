6f994dd02f7d8c971e46c9ecb15059218da012c4
Hiren <hiren7575@gmail.com>
2024-10-02 13:41:45 -0400

    pom updated, basepage added

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,5 +1,14 @@
 package pageObjects;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
 
+public class HomePage extends BasePage{
+
+
+	public HomePage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+	
 }
